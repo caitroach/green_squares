060c50920325feca517bb1271b9670118576fe79
cait <155276726+caitroach@users.noreply.github.com>
2026-03-07 11:46:11 -0500

    added dumb commit messages and quotes

--- commits.py
+++ commits.py
@@ -2,7 +2,10 @@
 people on github are LYING TO YOU!!!!!
 i too have been intimidated by large arrays of green squares, but it's really easy to automate and fake github activity.
 i'm keeping this public as a demonstration but here's how it's done ! ! !! ! I am not trying to con you
-but some people are........
+but some people are........ this is an expose 
+i'm gonna test it on my repo then probably turn it off
+maybe
+sorry
 '''
 
 import os 
@@ -11,13 +14,66 @@ import datetime
 import json 
 import subprocess
 
-quotes = [
+
+quotes = [ # various selected tweets, bumper stickers, and thoughts
     "I am batman",
-    "does this work?",
+    "just remember that you are ENOUGH. we don't need any more of you.",
+    "if at first you don't succeed, skydiving is not for you.",
+    "every dead body on mt everest was once highly motivated. being lazy could save your life. stay lazy.",
+    "ricky gervais is horrificially overrated imo",
+    "i'm not really sure how many quotes to put in here",
+    "the repo I'm looking at had a lot but",
+    "i think maybe it was vibe coded because that's way too many emojis",
+    "just a ton of emojis",
+    "or maybe the plural of emoji is emoji?",
+    ":3",
+    "meow",
+    "mnemotic to memorize the great lakes: Leslie Likes Licking Lettuce Lightly (Lake Ontario, Lake Huron, Lake Erie, Lake Michigan, Lake... whatever the last one is)",
+    "I'm an adult, i should know what the great lakes are",
+    "if anyone reads this it'll look really unprofessional",
+    "sorry whoever is reading this",
+    "i am but a humble nigiri on the rotating sushi conveyor belt of life",
+    "ios11",
+    "brought to you by clippy",
+    "visit https://cait.lol (BUT NOT ON MOBILE PLEASE I DON'T KNOW REACT YET)",
+    "though tbh maybe vanilla HTML/CSS/JS is sometimes harder than just giving up and learning react",
+    "meow again",
+    "keep honking! i'm listening to honking", 
+    "honk if you honk",
+    "no baby on board so feel free to hit me",
+    "bigfoot saw me and nobody believed her", 
+    "criminalize bumper stickers",
+    "red delicious is, like, the least delicious apple",
+    "honeycrisp and granny smith for life!",
+    "actually who the hell is granny smith? what a diva. love her",
+    "my other car is another car with a sticker on the car that says this my other car",
+    "don't like my driving? STAY OFF THE SIDEWALK!",
+    "jk i don't advocate for violence against pedestrians. i am more often a pedestrian than i am a driver tbh",
+    "nothing bad can happen. it can only good happen"
+
+
 ]
 
 commit_messages = [
-    "I am but a liar"
+    "I am but a liar",
+    "commit message",
+    "am I a human?",
+    "do automated commit messages dream of electric sheep?",
+    "gamestop2themoon!",
+    "idk how to parallel park",
+    "committing commits",
+    "commitment issues",
+    "sent emails",
+    "ran for mayor",
+    "assassinated a political figure",
+    "won the olympics",
+    "we love you Alysa Liu!",
+    "cait wasn't here",
+    "this is how people lie",
+    "LIED",
+    "throwing away your morals for a green box",
+    "LCDSOUNDSYSTEM2026!"
+    # i cant think of any more tbh
 ]
 
 target_files = ["daily_log.txt", "progress.md", "inspiration.txt"]
